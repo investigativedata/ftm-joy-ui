781c6ea8ed05590cd9702204be372ad037b22d37
Simon Wörpel <simon.woerpel@pm.me>
2023-10-16 14:29:12 +0200

    ✨ (util) add createEntityDict helper function

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,6 @@
 import defaultModel from "../data/defaultModel.json";
 import { Model } from "../model/model";
-import type { Entity, TEntity, TSchema } from "../types";
+import type { Entity, TEntity, TSchema, IEntityDatum } from "../types";
 
 const model = new Model(defaultModel);
 
@@ -12,4 +12,26 @@ export function getSchema(
   schemaName: string | null | undefined | TSchema
 ): TSchema {
   return model.getSchema(schemaName);
+}
+
+type EntityDict = {
+  [id: string]: Entity;
+};
+
+export function createEntityDict(
+  entities: IEntityDatum[],
+  lookups?: string[]
+): EntityDict {
+  const data: EntityDict = {};
+  for (const entity of entities.map(getProxy)) {
+    if (lookups) {
+      const keys = lookups.map((l) => entity.getFirst(l)).filter((k) => !!k);
+      if (keys.length > 0) {
+        data[keys.join("-")] = entity;
+      }
+    } else {
+      data[entity.id] = entity;
+    }
+  }
+  return data;
 }
